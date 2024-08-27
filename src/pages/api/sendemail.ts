9ac2d30746ed50bf8ad1561f947e9ba24a764b29
Bruno Sette <brunosette@gmail.com>
2024-08-27 16:26:25 -0400

    refactor: Update sendemail.ts to optimize email sending process

--- src/pages/api/sendemail.ts
+++ src/pages/api/sendemail.ts
@@ -5,7 +5,6 @@ export const POST: APIRoute = async ({ request }): Promise<Response> => {
   console.log("Received POST request to /api/sendemail");
 
   try {
-    // Ensure SendGrid API key is available
     const SENDGRID_API_KEY = import.meta.env.SENDGRID_API_KEY;
     if (!SENDGRID_API_KEY) {
       console.error("SENDGRID_API_KEY is not set");
@@ -37,10 +36,10 @@ export const POST: APIRoute = async ({ request }): Promise<Response> => {
       utm_campaign,
     } = data;
 
-    // Prepare email message
     const msg = {
-      to: "brunosette@gmail.com", // Your email address
-      from: "marketing@omnireno.ca", // Verified sender
+      to: "brunosette@omnireno.ca",
+      from: "marketing@omnireno.ca",
+      cc: "omnireno@robot.zapier.com",
       subject: "New Contact Form Submission",
       text: `
         Name: ${name}
@@ -55,16 +54,12 @@ export const POST: APIRoute = async ({ request }): Promise<Response> => {
       `,
     };
 
-    console.log("Attempting to send email with data:", msg);
-
     // Send email using SendGrid
     const [response] = await sgMail.send(msg);
     console.log("SendGrid API Response:", response);
 
     // Check response status
     if (response.statusCode >= 200 && response.statusCode < 300) {
-      console.log("Email sent successfully");
-
       return new Response(
         JSON.stringify({
           message: "Email sent successfully",
